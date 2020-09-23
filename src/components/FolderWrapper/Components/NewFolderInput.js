ff00479ef016f9e4ec95bccc446a084eda0007e7
Amit Kumar <besttechamit@gmail.com>
2020-09-23 17:45:06 +0530

    New Folder Input Function added to the Application

---
+++ src/components/FolderWrapper/Components/NewFolderInput.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './FolderWrapper.module.scss';
+
+class NewFolderInput extends React.Component {
+    validateFolderName = (e) =>{
+       const { addFolderFn, toggleFolderCreationFn } = this.props
+       const name = e.target.hljs-value
+       if( name !== ''){
+           addFolderFn({
+               name:name,
+               url: name.toLowerCase()
+           })
+       }
+       
+       toggleFolderCreationFn()
+    }
+
+    render() {
+        return(
+            <div className={styles.input}>
+                <input
+                autoFocus
+                className = {styles.input}
+                onBlur={this.validateFolderName}
+                placeholder='New Folder'
+                type='text'
+                />
+            </div>
+        )
+    }
+}
+
+NewFolderInput.propTypes = {
+    addFolderFn: PropTypes.func.isRequired,
+    toggleFolderCreationFn: propTypes.func.isRequired
+}
+
+export default NewFolderInput;
