986ea0bac40bbed5403535033fb5613b3794b126
Amit Kumar <besttechamit@gmail.com>
2020-09-24 21:19:19 +0530

    Thumbnail started

---
+++ src/components/NotesWrapper/components/NoteThumbnail.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
+
+import NotesContext from '../../../utils/context'
+
+import styles from '../NotesWrapper.module.scss'
+
+const NoteThumbNail = ({
+    note, 
+    selectNoteFn
+}) => {
+    <NotesContext.Consumer>
+        {
+            context => {
+                <Link
+                
+            }
+        }
+    </NotesContext.Consumer>
+}
