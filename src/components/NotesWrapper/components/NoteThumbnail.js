26405c6a870cc46d761469cbeb1cab6aa105d6e3
Amit Kumar <besttechamit@gmail.com>
2020-09-25 19:21:20 +0530

    ThumbNail functions updated!

--- src/components/NotesWrapper/components/NoteThumbnail.js
+++ src/components/NotesWrapper/components/NoteThumbnail.js
@@ -7,16 +7,46 @@ import NotesContext from '../../../utils/context'
 
 import styles from '../NotesWrapper.module.scss'
 
-const NoteThumbNail = ({
-    note, 
-    selectNoteFn
-}) => {
-    <NotesContext.Consumer>
-        {
-            context => {
-                <Link
-                
+const NoteThumbnail = ({
+  note,
+  selectNoteFn
+}) => (
+  <NotesContext.Consumer>
+    {
+      context => (
+        <Link
+          className={note.id === context.current.note ? styles.thumbnailActive : styles.thumbnail}
+          to={`/notes-app/${note.folder}/${note.id}`}
+          onClick={() => selectNoteFn(note.id)}
+        >
+          <>
+            {note.content ? (
+              <p className={styles.thumbnailHeader}>
+                {note.content.length > 25 ? note.content.slice(0, 25) + '...' : note.content}
+              </p>
+            ) : (
+              <p className={styles.thumbnailHeaderEmpty}>Empty note</p>
+            )}
+            <p className={styles.thumbnailDate}>
+              {moment(note.editDate).format('DD.MM.YYYY')}
+            </p>
+            {
+              context.current.folder === 'notes' &&
+                <div className={styles.thumbnailFolder}>
+                  <i className='fa fa-folder-o' />
+                  <span>{note.folder}</span>
+                </div>
             }
-        }
-    </NotesContext.Consumer>
+          </>
+        </Link>
+      )
+    }
+  </NotesContext.Consumer>
+)
+
+NoteThumbnail.propTypes = {
+  note: PropTypes.object.isRequired,
+  selectNoteFn: PropTypes.func.isRequired
 }
+
+export default NoteThumbnail
